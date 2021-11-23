635c561fde84790e0f9d670bb2b66892c80598c4
Emilio Ramirez <73008381+many1026@users.noreply.github.com>
2021-11-23 14:54:14 -0600

    Update Create.sql

--- Sql/Create.sql
+++ Sql/Create.sql
@@ -35,17 +35,26 @@ create table dispositivos_cuales (
 	id_demograficos int4 references vulnerabilidad (id_vulnerabilidad) not null, 
 	dispositivos_cantidad text
 );
+create table exploratorias (
+	id_exploratorias int4 primary key,
+	id_demograficos int4 references demograficos (id_demograficos) not null,
+	seguridad float8,
+	privacidad text,
+	huella text
+)
 
 create table manejo_info (
-id_manejo_info int4 primary key,
+	id_manejo_info int4 primary key,
+	id_demograficos int4 references demograficos (id_demograficos) not null,
 	datos text,
 	empresas text,
 	preocupacion_empresas text,
 	username text
-);
+)
 
 create table prevencion_digital (
-id_prevencion_digital int4 primary key,
+	id_prevencion_digital int4 primary key,
+	id_demograficos int4 references demograficos (id_demograficos) not null,
 	banca text,
 	compras_online text,
 	paypal text,
@@ -56,21 +65,30 @@ id_prevencion_digital int4 primary key,
 	cargos text,
 	con_victima text,
 	evitado text
-);
+)
 
 create table prevencion_navegacion (
-id_prevencion_navegacion int4 primary key,
+	id_prevencion_navegacion int4 constraint pk_prevencion_navegacion primary key,
+	id_demograficos int4 references demograficos (id_demograficos) not null,
 	navegador text,
 	url text,
 	phishing text
-);
+)
 
 create table proteccion(
-	id_proteccion int4 primary key, 
+	id_proteccion int4 constraint pk_proteccion primary key, 
+	id_prevencion_navegacion int4 references prevencion_navegacion (id_prevencion_navegacion) not null,
 	proteccion text 
-);
+)
+
+create table victima( 
+	id_victima int4 constraint pk_victima primary key, 
+	id_prevencion_navegacion int4 references prevencion_navegacion (id_prevencion_navegacion) not null,
+	victima text
+)
 create table redes_sociales (
-	id_redes_sociales int4 primary key, 
+	id_redes_sociales int4 constraint pk_redes_sociales primary key, 
+	id_demograficos int4 references demograficos (id_demograficos) not null,
 	sociales_contar float8, 
 	ordena_facebook text, 
 	ordena_instagram text, 
@@ -79,13 +97,9 @@ create table redes_sociales (
 	ordena_tiktok bool, 
 	ordena_ninguna text, 
 	ordena_otra bool
-);
+)
 create table sociales_cuales(
-	id_sociales_cuales int4 primary key, 
+	id_sociales_cuales int4 constraint pk_sociales_cuales  primary key, 
+	id_redes_sociales int4 references redes_sociales (id_redes_sociales) not null,
 	sociales_cuales text
-);
-create table victima( 
-	id_victima int4 primary key, 
-	victima text
-);
-
+)
